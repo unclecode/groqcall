a73b40d8d603b4dda1e44fbfa766803cc70fed7b
unclecode <unclecode@kidocode.com>
2024-03-14 15:29:06 +0800

    Improve error handling and logging for "No tools found" issue
    - Enhance the parsing prompt to better detect tools and reduce the occurrence of the "Error: 500 {"tool_calls":[]}" issue
    - Implement a new logger to track incidents when no tools are found for a "tool-request"
    - Update the error message to provide more clarity on the potential causes of the issue
    - Rename the issue title to "No tools found" for better clarity and tracking
    
    The improvements in the parsing prompt have shown positive results, but further monitoring and refinement are necessary. The new logger will help in identifying and investigating future occurrences of the "No tools found" issue, enabling targeted improvements to the PROMPT.
    
    This commit addresses the ongoing issue, but the problem will remain open for further observation and enhancements.

--- app/utils.py
+++ app/utils.py
@@ -2,15 +2,45 @@ import logging
 import os
 
 # To be developed
-def create_logger(log_path:str = ".logs/access.log"):
+def create_logger(logger_name: str, log_path: str = ".logs/access.log"):
     log_dir = os.path.dirname(log_path)
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
-    logger = logging.getLogger("app")
+    logger = logging.getLogger(logger_name)
     logger.setLevel(logging.DEBUG)
     file_handler = logging.FileHandler(log_path)
     file_handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
     return logger
+
+
+def get_tool_call_response( completion, unresolved_tol_calls, resolved_responses):
+    tool_response = {
+        "id": "chatcmpl-" + completion.id,
+        "object": "chat.completion",
+        "created": completion.created,
+        "model": completion.model,
+        "choices": [
+            {
+                "index": 0,
+                "message": {
+                    "role": "assistant",
+                    "content": None,
+                    "tool_calls": unresolved_tol_calls,
+                },
+                "logprobs": None,
+                "finish_reason": "tool_calls",
+            }
+        ],
+        "resolved": resolved_responses,
+        "usage": {
+            "prompt_tokens": completion.usage.prompt_tokens,
+            "completion_tokens": completion.usage.completion_tokens,
+            "total_tokens": completion.usage.total_tokens,
+        },
+        "system_fingerprint": completion.system_fingerprint,
+    }    
+
+    return tool_response
