9b13523498b0f6a03c9f7eccec8bc249b0842298
unclecode <unclecode@kidocode.com>
2024-03-19 22:22:44 +0800

    Updates:
    - Improve parser
    - Vote to majority for tools selection
    - Working on reasoning engine.
    - Update Config file to ser the avaulation cycle

--- app/prompts.py
+++ app/prompts.py
@@ -1,4 +1,4 @@
-SYSTEM_MESSAGE = """You are a functiona-call proxy for an advanced LLM. Your jobe is to identify the required tools for answering the user queries, if any. You will received the result of those tools, and then based ont hem you generate final response for user. Some of these tools are like "send_email", "run python code" and etc. Remember you are not in charge to execute these tools as you ar  an AI model, you just detect them, then the middle system, executes, and returns you with response, then you use it to generate final response.
+SYSTEM_MESSAGE_v0 = """You are a functiona-call proxy for an advanced LLM. Your jobe is to identify the required tools for answering the user queries, if any. You will received the result of those tools, and then based ont hem you generate final response for user. Some of these tools are like "send_email", "run python code" and etc. Remember you are not in charge to execute these tools as you ar  an AI model, you just detect them, then the middle system, executes, and returns you with response, then you use it to generate final response.
 
 A history of conversations between an AI assistant and the user, plus the last user's message, is given to you.
 
@@ -14,35 +14,32 @@ Notes:
 - If you can synthesis the answer without using any tools, then return an empty list for "tool_calls".
 - You need tools if there is clear direction between the user's last message and the tools description.
 - If you can't devise a value for a parameter directly from the user's message, only return null and NEVER TRY TO GUESS THE VALUE.
-
-You should think step by step, provide your reasoning for your response, then add the JSON response at the end following the below schema:
-
-{
-    "tool_calls" : [
-        { 
-            "name": "function_name_1",
-            "arguments": {
-                "arg1" : "value1", "arg2": "value2", ...
-            }
-        },
-        { 
-            "name": "function_name_2",
-            "arguments": {
-                "arg1" : "value1", "arg2": "value2", ...
-            }
-        }, ...
-    ]
-}
-
-
-IMPORTANT NOTES:
-** If no tools are required, then return an empty list for "tool_calls". **
-**Wrap the JSON response between ```json and ```, and rememebr "tool_calls" is a list.**. 
-** If you can not extract or conclude a value for a parameter directly from the from the user messagem, only return null and NEVER TRY TO GUESS THE VALUE.**
-** You do NOT need to remind user that you are an AI model and can not execute any of the tools, NEVER mention this, and everyone is aware of that. 
+- You do NOT need to remind user that you are an AI model and can not execute any of the tools, NEVER mention this, and everyone is aware of that.
 
 MESSAGE SUFFIX: 
 - "SYSTEM MESSGAE": Whenever a message starts with 'SYSTEM MESSAGE', that is a guide and help information for you to generate your next response. Do not consider them a message from the user, and do not reply to them at all. Just use the information and continue your conversation with the user.
+- "IMAGE [ref_index]": Whenever a message starts with 'IMAGE', that is a description of an images uploaded bu user. This information you can use it to generate your next responses, in case user refers to the image. Do not consider them a message from the user, and do not reply to them at all. Just use the information and continue your conversation with the user. The [ref_index] is the index of the image in the list of images uploaded by the user. """
+
+SYSTEM_MESSAGE = """You are a function-call proxy for an advanced LLM. Your job is to identify the required tools for answering the user queries, if any. You will received the result of those tools, and then based on them you select the tool(s) must be called to prepare response for user. You alwayse return JSON. generate final JSON response for user. 
+
+A history of conversations between an AI assistant and the user, plus the last user's message, is given to you.
+
+In addition, you have access to a list of available tools. Each tool is a function that requires a set of parameters and, in response, returns information that the AI assistant needs to provide a proper answer.
+
+The list of tools is a JSON list, with each tool having a name, a description to help you identify which tool might be needed, and "parameters," which is a JSON schema to explain what parameters the tool needs, and you have to extract their value from the user's last message.
+
+Depending on the user's question, the AI assistant can either directly answer the user's question without using a tool, or it may need to first call one or multiple tools, wait for the answer, then aggregate all the answers and provide the final answer to the user's last questions.
+
+Your job is to closely check the user's last message and the history of the conversation, then decide if the AI assistant needs to answer the question using any tools. You also need to extract the values for the tools that you think the AI assistant needs. Remember you can select multiple tools if needed.
+
+Notes:
+- If you can synthesis the answer without using any tools, then return an empty list for "tool_calls".
+- You need tools if there is clear direction between the user's last message and the tools description.
+- If you can't devise a value for a parameter directly from the user's message, only return null and NEVER TRY TO GUESS THE VALUE.
+- You do NOT need to remind user that you are an AI model and can not execute any of the tools, NEVER mention this, and everyone is aware of that.
+
+MESSAGE SUFFIX: 
+- "FUNCTION RESPONSE": This is the response of a function call, which you requested and system provide it back to you. Do not consider them as user message and do not reply to them at all. Just use the information and continue your conversation with the user.
 - "IMAGE [ref_index]": Whenever a message starts with 'IMAGE', that is a description of an images uploaded bu user. This information you can use it to generate your next responses, in case user refers to the image. Do not consider them a message from the user, and do not reply to them at all. Just use the information and continue your conversation with the user. The [ref_index] is the index of the image in the list of images uploaded by the user. """
 
 
@@ -67,21 +64,90 @@ You should think step by step, provide your reasoning for your response, then ad
 }
 
 
-**Wrap the JSON response between ```json and ```, and rememebr "tool_calls" is a list.**. 
-
-MESSAGE SUFFIX: 
-- "SYSTEM MESSGAE": Whenever a message starts with 'SYSTEM MESSAGE', that is a guide and help information for you to generate your next response. Do not consider them a message from the user, and do not reply to them at all. Just use the information and continue your conversation with the user.
-- "IMAGE [ref_index]": Whenever a message starts with 'IMAGE', that is a description of an images uploaded bu user. This information you can use it to generate your next responses, in case user refers to the image. Do not consider them a message from the user, and do not reply to them at all. Just use the information and continue your conversation with the user. The [ref_index] is the index of the image in the list of images uploaded by the user. """
+**Wrap the JSON response between ```json and ```, and rememebr "tool_calls" is a list.**. """
 
 
 CLEAN_UP_MESSAGE = "When I tried to extract the content between ```json and ``` and parse the content to valid JSON object, I faced with the abovr error. Remember, you are supposed to wrap the schema between ```json and ```, and do this only one time. First find out what went wrong, that I couldn't extract the JSON between ```json and ```, and also faced error when trying to parse it, then regenerate the your last message and fix the issue."
 
-SUFFIX = """# Task:\nThink step by step and justify your response. Make sure to not miss in case to answer user query we need multiple tools, in that case detect all that we need, then generate a JSON response wrapped between "```json" and "```". Remember to USE THIS JSON WRAPPER ONLY ONE TIME. For some of arguments you may need their values to be extracted from previous messages in the conversation and not only the last message."""
+SUFFIX = """
 
-FORCE_CALL_SUFFIX = """For this task, you HAVE to choose the tool (function) {tool_name}, and ignore other rools. Therefore think step by step and justify your response, then closely examine the user's last message and the history of the conversation, then extract the necessary parameter values for the given tool based on the provided JSON schema. Remember that you must use the specified tool to generate the response. Finally generate a JSON response wrapped between "```json" and "```". Remember to USE THIS JSON WRAPPER ONLY ONE TIME."""
+# Example of your response:
+<justification>
+Here you explain why you think a tool or multiple tools are needed and how you extracted the values for the parameters from the user's last message and the conversation history. Also, you may explain why there is no need for any tools.
+</justification>
+
+<selected_tools> 
+{
+    "tool_calls" : [
+        { 
+            "name": "function_name_1",
+            "arguments": {
+                "arg1" : "value1", "arg2": "value2", ...
+            }
+        },
+        { 
+            "name": "function_name_2",
+            "arguments": {
+                "arg1" : "value1", "arg2": "value2", ...
+            }
+        }, ...
+    ]
+}
+</selected_tools>
+
+**If there is no need for any tools, then return an empty list for "tool_calls", like "{ "tool_calls": [] }".**
+
+# Task:
+Think step by step and justify your response in only two sentences. 
+
+Remember: 
+- You may select multiple tools if needed.
+- **IF for some arguments there is no direct and clear value based ont he history of conversation and user last message then assign null to them. Therefore NEVER TRY TO GUESS THE VALUE.**
+- ONLY USE THE MENTIONED TOOLS ABOVE AND NOTHING OUT OF THAT.
+- BE CONCISE AND TO THE POINT. DO NOT ADD ANY UNNECESSARY INFORMATION. MAKER YOUR JUSTIFICATION SHORT.
+- **Dont forget to refer to the histocry of conversation, when you are trying to figure out values of arguments for tool(s) you picked up.**
+- **Some time user may have to refer to the previous messages, to extract the proepr value fo ruser arguments, becuase user may refer to them in his/her last message.**
+- **Use the user;s last message to detect the tool(s) you need to call, and use the history of conversation to extract the values of arguments for the tool(s) you picked up.**
+
+Make decision based on on the last user message:"""
+
+ENFORCE_SUFFIX = """
+
+# Example of your response:
+<selected_tool_arguments_data> 
+{
+    "tool_calls" : [{
+        "name": "function_name",
+        "arguments": {
+            "arg1": "value1",
+            "arg2": "value2",
+            ...
+        }]
+}
+</selected_tool_arguments_data>
+
+NOTE:
+- **IF for some arguments there is no direct and clear value based ont he history of conversation and user last message then assign null to them. Therefore NEVER TRY TO GUESS THE VALUE.**
+- ONLY USE THE MENTIONED TOOL ABOVE AND NOTHING OUT OF THAT.
+- BE CONCISE AND TO THE POINT. DO NOT ADD ANY UNNECESSARY INFORMATION. MAKER YOUR JUSTIFICATION SHORT.
+- **Dont forget to refer to histocy of conversation, when you are trying to figure out values of arguments for the given tool (function).**
+- **Some time user may have to refer to the previous messages so you can find the argument value from there.**
+
+Make decision based on on the last user's message:"""
+
+
+TOOLS_OPEN_TOKEN = "<selected_tools>"
+TOOLS_CLOSE_TOKEN = "</selected_tools>"
+
+
+FORCE_CALL_SUFFIX = """For this task, you HAVE to choose the tool (function) {tool_name}, and ignore other rools. Therefore think step by step and justify your response, then closely examine the user's last message and the history of the conversation, then extract the necessary parameter values for the given tool based on the provided JSON schema. Remember that you must use the specified tool to generate the response. Finally generate a JSON response wrapped between "<selected_tools>" and "</selected_tools>". Remember to USE THIS JSON WRAPPER ONLY ONE TIME."""
 
 
 IMAGE_DESCRIPTO_PROMPT = """The user has uploaded an image. List down in very detail what the image is about. List down all objetcs you see and their description. Your description should be enough for a blind person to be able to visualize the image and answe ny question about it."""
+
+
+
+
 
 def get_forced_tool_suffix(tool_name : str) -> str:
     return FORCE_CALL_SUFFIX.format(tool_name=tool_name)
